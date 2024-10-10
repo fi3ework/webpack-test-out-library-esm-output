9c019ac098ed2733aa8861a30b79ea475bd7636a
fi3ework <fi3ework@gmail.com>
2024-10-11 02:37:09 +0800

    fix fix fix

--- packages/webpack-18843/src/index.js
+++ packages/webpack-18843/src/index.js
@@ -1,4 +1,13 @@
-import other from './other'
+import x from './module.js'
 
-const Symbol = 'Symbol'
-export const a = 1 + other
+console.log(x)
+
+const localVar = 1
+
+it('should not leak localVar to other modules', () => {
+  const src_localVar = 42
+  const through_variable = typeof localVar
+  function test() {
+    const v1 = 1
+  }
+})
